0a4fb98c9bfe2d544bb7405b9551854feb49f442
Shree2124 <shreealasande@gmail.com>
2026-02-20 13:14:42 +0530

    fix

--- src/mail.js
+++ src/mail.js
@@ -1,7 +1,10 @@
 import nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
   service: "gmail",
+  port: 587,
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
